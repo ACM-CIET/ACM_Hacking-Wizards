311686bf2bc95749ce8aa3dab3cca4163c510b66
Chitkara ACM Student Chapter <146860353+ACM-CIET@users.noreply.github.com>
2023-10-14 20:03:15 +0530

    Delete src/navbar.js

--- src/navbar.js
+++
@@ -1,8 +0,0 @@
-// NAVBAR resopnsiveness
-
-const toggleButton = document.getElementsByClassName('toggle_button')[0]
-const navbarlinks = document.getElementsByClassName('links')[0]
-
-toggleButton.addEventListener('click', () => {
-    navbarlinks.classList.toggle('active')
-})
